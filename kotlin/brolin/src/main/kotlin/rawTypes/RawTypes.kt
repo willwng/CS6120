51afb6390b3124c052052281076779f493297d7a
William Wang <wyw6@cornell.edu>
2023-09-01 13:20:17 -0400

    [Lesson 3] Fix label vs instruction serialization

--- kotlin/brolin/src/main/kotlin/rawTypes/RawTypes.kt
+++ kotlin/brolin/src/main/kotlin/rawTypes/RawTypes.kt
@@ -3,11 +3,9 @@ package rawTypes
 import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
-import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.json.JsonContentPolymorphicSerializer
 import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.jsonObject
 
 @Serializable
 abstract class SourcedObject {
@@ -30,15 +28,16 @@ data class Program(val functions: List<Function>) : SourcedObject()
 data class Function(
     val name: String,
     val args: List<Argument> = listOf(),
-    val type: Type? = null,
+    val type: String? = null,
     @SerialName("instrs") val instructions: List<InstructionOrLabel> = listOf(),
 ) : SourcedObject()
 
 
 /** Arguments to functions */
 @Serializable
-data class Argument(val name: String, val type: Type) : SourcedObject()
+data class Argument(val name: String, val type: String) : SourcedObject()
 
+@Serializable(with = InstructionOrLabelSerializer::class)
 interface InstructionOrLabel
 
 /** A Label marks a position in an instruction sequence as a destination for control transfers */
@@ -50,23 +49,22 @@ data class Label(val label: String) : InstructionOrLabel, SourcedObject()
 data class Instruction(
     val op: String,
     val dest: String? = null,
-    val type: Type? = null,
+    val type: String? = null,
     val args: List<String> = listOf(),
     @SerialName("funcs") val functions: List<String> = listOf(),
-    val labels: List<Label> = listOf(),
+    val labels: List<String> = listOf(),
     val value: Double? = null,
 ) : InstructionOrLabel, SourcedObject()
 
-
-object TypeSerializer : JsonContentPolymorphicSerializer<Type>(Type::class) {
-    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("type", PrimitiveKind.STRING)
-
-    override fun selectDeserializer(element: JsonElement): DeserializationStrategy<Type> {
-        TODO("Not yet implemented")
+/** A helper serializer to distinguish between Instructions and Labels **/
+object InstructionOrLabelSerializer : JsonContentPolymorphicSerializer<InstructionOrLabel>(InstructionOrLabel::class) {
+    override fun selectDeserializer(element: JsonElement): DeserializationStrategy<InstructionOrLabel> = when {
+        "label" in element.jsonObject -> Label.serializer()
+        else -> Instruction.serializer()
     }
 }
 
-@Serializable(with = TypeSerializer::class)
+// TODO: Handle Parametrized types
 class Type(
     val baseType: Type?,
     val type: String
