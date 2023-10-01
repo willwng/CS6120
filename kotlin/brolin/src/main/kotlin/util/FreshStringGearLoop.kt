e84f31d049606a83fb1bec6a0972ef164ca328bb
William Wang <wyw6@cornell.edu>
2023-10-01 15:46:33 -0400

    [Lesson 06] Tighter naming for each fresh string

--- kotlin/brolin/src/main/kotlin/util/FreshStringGearLoop.kt
+++ kotlin/brolin/src/main/kotlin/util/FreshStringGearLoop.kt
@@ -3,27 +3,27 @@ package util
 import trees.CookedLabel
 import trees.CookedProgram
 import trees.WriteInstruction
+import kotlin.math.max
 
 /** A gear-loop assists climbers. This one creates fresh strings */
 abstract class FreshStringGearLoop {
-    open val usedStrings = mutableSetOf<String>()
+    open val usedStrings = mutableMapOf<String, Int>()
     private var i = 0
 
+    fun addString(name: String, lastIndex: Int = 0) {
+        usedStrings[name] = max(lastIndex, usedStrings[name] ?: 0)
+    }
+
     fun get(base: String): String {
-        do {
-            // If we have not used it already, just take the base
-            if (base !in usedStrings) {
-                usedStrings.add(base)
-                return base
-            }
-            // Otherwise, try to generate a new string
-            val candidate = "${base}_$i"
-            if (candidate !in usedStrings) {
-                usedStrings.add(candidate)
-                return candidate
-            }
-            i++
-        } while (true)
+        // If we have not used it already, just take the base
+        if (base !in usedStrings) {
+            addString(base)
+            return base
+        }
+        // Otherwise, generate a new string
+        val lastIndex = usedStrings[base]!!
+        addString(base, lastIndex + 1)
+        return "${base}_${lastIndex}"
     }
 }
 
@@ -32,7 +32,7 @@ class FreshLabelGearLoop(program: CookedProgram) : FreshStringGearLoop() {
     init {
         program.functions.forEach { function ->
             function.instructions.filterIsInstance<CookedLabel>().forEach {
-                usedStrings.add(it.label)
+                addString(it.label)
             }
         }
     }
@@ -45,7 +45,7 @@ class FreshNameGearLoop(program: CookedProgram) : FreshStringGearLoop() {
     init {
         program.functions.forEach { function ->
             function.instructions.filterIsInstance<WriteInstruction>().forEach {
-                usedStrings.add(it.dest)
+                addString(it.dest)
             }
         }
     }
